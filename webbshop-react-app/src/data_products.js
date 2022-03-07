093dcd1ef18f98e624d667416ed21477926a8033
SuarPha K <92083398+SuarPha@users.noreply.github.com>
2022-03-07 19:44:19 +0100

    Create data_products.js

---
+++ webbshop-react-app/src/data_products.js
@@ -0,0 +1,136 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://media.istockphoto.com/photos/pomegranate-cheesecake-picture-id1146489716?k=20&m=1146489716&s=612x612&w=0&h=vv4KqaV5kSZzeU4A9AWrO9jNHGBNqNPW1R5j156dWtI=",
+    title: "LETS'T PARTY!",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f6f6f6",
+  },
+  {
+    id: 2,
+    img: "https://media.istockphoto.com/photos/woman-setting-up-cake-table-outdoors-picture-id1248736000?k=20&m=1248736000&s=612x612&w=0&h=65wdc_sZvkWWKoG92_O_ru6dpcSptlGzNVAMsDBIrT4=",
+    title: "BE COOL BE YOU!",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://media.istockphoto.com/photos/woman-taking-a-slice-of-cake-homemade-rainbow-cake-on-blue-background-picture-id1288470137?k=20&m=1288470137&s=612x612&w=0&h=ioL82C_hrGryo_IYzvsQaABN7BySO_Dya0g9Cherk6g=",
+    title: "SHOES FOR ANY SEASON!",
+    description:
+      "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f6f6f6",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://media.istockphoto.com/photos/sliced-strawberry-cheesecake-gelatine-dessert-with-strawberry-picture-id1222074976?k=20&m=1222074976&s=612x612&w=0&h=eqOVtPsg7PHFVfeV6ZO4ojW-B_xWTFv8f_ZouYTc-NQ=",
+    title: "STRAWBERRY",
+  },
+  {
+    id: 2,
+    img: "https://media.istockphoto.com/photos/happy-mothers-day-greeting-card-with-heart-cheesecake-with-picture-id930940222?k=20&m=930940222&s=612x612&w=0&h=u2VRq4qsn0t6M5qhrCfot0Ohg0rT-TAs70n9NGbkdQg=",
+    title: "FOOTWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://media.istockphoto.com/photos/slice-missing-from-chocolate-cake-on-cakestand-picture-id103332768?k=20&m=103332768&s=612x612&w=0&h=cungK41bJfSqYguuZTXQoW3Us5MEOzALRDXWdQq3hd4=",
+    title: "MEN MOOD",
+  },
+];
+export const popularItem = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/1038711/pexels-photo-1038711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/691147/pexels-photo-691147.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  },
+  {
+    id: 3,
+    img: "https://cdn.pixabay.com/photo/2016/10/27/22/12/cake-1776661_1280.jpg",
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/2531546/pexels-photo-2531546.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  },
+  {
+    id: 5,
+    img: "https://images.pexels.com/photos/2510104/pexels-photo-2510104.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+];
+
+export const productList = [
+  {
+    id: 234967,
+    title: "Wedding cake",
+    description:
+      "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n recusandae alias error harum maxime adipisci amet laborum.",
+    price: 12500,
+    currency: "$",
+    storage: 3,
+    url: "https://images.pexels.com/photos/1038711/pexels-photo-1038711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+
+  {
+    id: 678453,
+    title: "Chocolate cake",
+    description:
+      "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n recusandae alias error harum maxime adipisci amet laborum.",
+    price: 899,
+    currency: "$",
+    storage: 91,
+    url: "https://images.pexels.com/photos/691147/pexels-photo-691147.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  },
+
+  {
+    id: 972342,
+    title: "Strawberry cake",
+    description:
+      "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n recusandae alias error harum maxime adipisci amet laborum.",
+    price: 850,
+    currency: "$",
+    storage: 234,
+    url: "https://cdn.pixabay.com/photo/2016/10/27/22/12/cake-1776661_1280.jpg",
+  },
+
+  {
+    id: 1467325,
+    title: "Birthday cake",
+    description:
+      "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n recusandae alias error harum maxime adipisci amet laborum.",
+    price: 1500,
+    currency: "$",
+    storage: 32,
+    url: "https://images.pexels.com/photos/2531546/pexels-photo-2531546.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+  },
+
+  {
+    id: 5456463,
+    title: "Fruit cake",
+    description:
+      "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n recusandae alias error harum maxime adipisci amet laborum.",
+    price: 1930,
+    currency: "$",
+    storage: 11,
+    url: "https://images.pexels.com/photos/2510104/pexels-photo-2510104.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+];
+
+export const specificProduct = [
+  {
+    id: 234967,
+    url: "https://images.pexels.com/photos/1038711/pexels-photo-1038711.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "Wedding cake",
+    description:
+      "Quo neque error repudiandae fuga? Ipsa laudantium molestias eos \n sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam\n recusandae alias error harum maxime adipisci amet laborum.",
+    price: 12500,
+    currency: "$",
+    storage: 3,
+  },
+];
